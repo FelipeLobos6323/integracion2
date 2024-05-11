fc454a9de0328833f3517109a996676b7252e02c
FelipeLobos6323 <f.lobos@duocuc.cl>
2024-05-11 19:20:40 -0400

    Agregando mas datos al models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Producto (models.Model):
@@ -9,5 +9,8 @@ class Producto (models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.CharField(max_length=10)
     fecha = models.CharField(max_length=10)
-    
+    disponible = models.BooleanField(default=False)
+    stock = models.IntegerField(default=0)
+
+
     
